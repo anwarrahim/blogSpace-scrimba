7b3217fcf2035ad540cc0f8104f5a06fb1ad7b62
Anwar Rahim <anwargithub@gmail.com>
2025-02-05 00:51:07 +0500

    Refactor submission form and implement post submission functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(data => {
@@ -18,6 +16,47 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         }
         document.getElementById('blog-content').innerHTML = html
     })
+
+
+
+    document.getElementById('post-form').addEventListener('submit', e=>{
+        e.preventDefault()
+        const postTitle = document.getElementById('post-title').value
+        const postBody = document.getElementById('post-body').value
+        const dataObj ={
+            title: postTitle,
+            body:postBody
+        }
+        
+
     
+        fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+            method:'POST',
+            body:JSON.stringify(dataObj),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+        
+            document.getElementById('blog-content').innerHTML =
+            `
+            <h1>
+                ${data.title}
+            </h1>
+               
+               <p>
+                ${data.body}
+               </p>
+               ${document.getElementById('blog-content').innerHTML}
+            `
+
+        })
+       
+    })
+    
+
+      
 
     
